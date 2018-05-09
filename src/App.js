4daa6aead8ef7ee2ef0bf8f82268b52822aa0236
qi <12meridiansmedia@gmail.com>
2018-05-09 13:32:28 +0300

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './green_app_icon.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        
+        <table className="header">
+          <tbody>
+            <tr>
+              <td className="header-logo">
+                <img 
+                  alt="icon" 
+                  src={logo}
+                  width="50px"
+                />
+              </td>
+              <td>
+                <h1>MoviesDB Search</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
       </div>
     );
   }
